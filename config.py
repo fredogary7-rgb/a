e685ca92393aa2eb04b17bf76c3287e98ab0d369
fredogary7-rgb <tonEmail@gmail.com>
2026-01-12 21:20:05 +0000

    Prepare deployment

--- config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-import os
-from dotenv import load_dotenv
-
-load_dotenv()  # Charge les variables d'environnement depuis .env
-
-BKAPAY_PUBLIC_KEY = os.getenv("BKAPAY_PUBLIC_KEY")
-BKAPAY_CALLBACK_URL = os.getenv("BKAPAY_CALLBACK_URL")  # <-- Bien présent
-BKAPAY_WEBHOOK_SECRET = os.getenv("BKAPAY_WEBHOOK_SECRET")
